6804b60e23c6636792faa64665361029b50d1e86
Vlad Temian <vladtemian@gmail.com>
2016-09-05 21:20:25 +0300

    Initial work by @AMecea.

---
+++ pl-lets-encrypt.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* Plugin Name: Presslabs Let's Encrypt
+* Description: This plugin is used by Presslabs to generate SSL certificates.
+* Author: Presslabs
+*
+* */
+
+function pl_acme_challenge() {
+	$oxygen_url = apply_filters( 'pl_acme_oxygen_url', 'https://o.presslabs.com' );
+	$oxygen_verify_cert = apply_filters( 'pl_acme_oxygen_verify_cert', true );
+	$challenge_url = "$oxygen_url/certificates/" . ltrim( $_SERVER['REQUEST_URI'], '/' );
+	$response = wp_remote_get( $challenge_url, array( 'sslverify' => $oxygen_verify_cert ) );
+	if ( ! is_wp_error( $response ) && $response['response']['code'] == 200 ) {
+		echo $response['body'];
+	} else {
+		trigger_error( 'ACME challenge failed. request: "' . $challenge_url . '", response: ' . json_encode( $response ), E_USER_ERROR );
+	}
+	die();
+}
+
+function pl_lets_encrypt() {
+    if ( substr( $_SERVER['REQUEST_URI'], 0, 28 ) == '/.well-known/acme-challenge/' ) {
+        pl_acme_challenge();
+    }
+}
+
+add_action('init', 'pl_lets_encrypt');
